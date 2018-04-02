fbb1af369c552b677aabecf5cdfedc350065500c
Nick Denry <nick.denry@gmail.com>
2018-04-02 20:39:15 +0300

    Add default map view i18n
    Set Google maps language to the composition one instead of fixed 'de'.

--- src/views/blocks/MapBlock.php
+++ src/views/blocks/MapBlock.php
@@ -8,6 +8,6 @@ $identifier = $this->extraValue('identifier', 0);
 ?>
 <?php if (!empty($this->varValue('address'))):?>
     <div class="embed-responsive embed-responsive-16by9">
-            <iframe src="<? if ($this->cfgValue('snazzymapsUrl', false)): ?><?= $this->cfgValue('snazzymapsUrl', '') ?><? else: ?>https://maps.google.com/maps?f=q&source=s_q&hl=de&geocode=&q=<?= $this->extraValue('address');?>&z=<?= $this->extraValue('zoom');?>&t=<?= $this->extraValue('maptype')?>&output=embed<? endif; ?>" width="600" height="450" frameborder="0" style="border:0"></iframe>
+            <iframe src="<? if ($this->cfgValue('snazzymapsUrl', false)): ?><?= $this->cfgValue('snazzymapsUrl', '') ?><? else: ?>https://maps.google.com/maps?f=q&source=s_q&hl=<?= \Yii::$app->composition['langShortCode']; ?>&geocode=&q=<?= $this->extraValue('address');?>&z=<?= $this->extraValue('zoom');?>&t=<?= $this->extraValue('maptype')?>&output=embed<? endif; ?>" width="600" height="450" frameborder="0" style="border:0"></iframe>
     </div>
 <?php endif; ?>
